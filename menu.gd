462ec0fe8cfcaf66bf7054949f8f19db063320a9
Serdgio1 <suprun_777@list.ru>
2024-08-09 22:05:24 +0300

    'menu'

---
+++ menu.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+var _texture1 = load("res://assets/Buttons Vector Animation Pack/about/72 px/about01.png")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_play_pressed():
+	pass # Replace with function body.
